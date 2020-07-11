cd5d77d8d725872ac82c805d28f7a1845be54dcb
Anda Turcanu <anda.turcanu@gmail.com>
2020-07-11 16:34:00 +0300

    projects structrue

---
+++ boot-microservice/blog-config-server/src/main/java/com/blog/ms/blogconfigserver/BlogConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.blog.ms.blogconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogConfigServerApplication.class, args);
+	}
+
+}
---
+++ boot-microservice/blog-service-registry/src/main/java/com/blog/ms/blogserviceregistry/BlogServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.blog.ms.blogserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogServiceRegistryApplication.class, args);
+	}
+
+}
